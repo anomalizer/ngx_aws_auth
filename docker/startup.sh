4b168691c954b4fa2c98cc3c18941b0c687c6cf7
Kory Taborn <Kory.Taborn@wnco.com>
2020-01-20 16:41:23 -0600

    add security token to nginx conf docker

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # substitute the domain and configure the first set of credentials
-envsubst '${BUCKET},${BUCKET_DOMAIN},${_AWS_SIGNING_SCOPE},${_AWS_ACCESS_KEY},${_AWS_SIGNING_KEY}' </tmp/nginx/server.conf > /tmp/nginx/server-tmp.conf
+envsubst '${BUCKET},${BUCKET_DOMAIN},${_AWS_SIGNING_SCOPE},${_AWS_ACCESS_KEY},${_AWS_SIGNING_KEY},${TOKEN}' </tmp/nginx/server.conf > /tmp/nginx/server-tmp.conf
 cp /tmp/nginx/server-tmp.conf /etc/nginx/server.conf
 
 # start nginx (blocking call)
