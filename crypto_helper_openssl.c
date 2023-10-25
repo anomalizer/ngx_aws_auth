151eb25d4b21dd11f29e9e168a75af2adc69a94c
gauravko <gaurav.gprs@gmail.com>
2023-10-25 19:53:00 +0530

    ubuntu 22.04 openssl compile error

--- crypto_helper_openssl.c
+++ crypto_helper_openssl.c
@@ -9,8 +9,6 @@
 
 #include "crypto_helper.h"
 
-#include <openssl/bio.h>
-#include <openssl/buffer.h>
 #include <openssl/evp.h>
 #include <openssl/hmac.h>
 #include <openssl/sha.h>
@@ -20,32 +18,38 @@ static const EVP_MD* evp_md = NULL;
 ngx_str_t* ngx_aws_auth__sign_sha256_hex(ngx_pool_t *pool, const ngx_str_t *blob,
     const ngx_str_t *signing_key) {
 
-    unsigned int      md_len;
-    unsigned char     md[EVP_MAX_MD_SIZE];
-	ngx_str_t *const retval = ngx_palloc(pool, sizeof(ngx_str_t));
+    unsigned int md_len;
+    unsigned char md[EVP_MAX_MD_SIZE];
+    ngx_str_t *const retval = ngx_palloc(pool, sizeof(ngx_str_t));
 
-    if (evp_md==NULL) {
-       evp_md = EVP_sha256();
+    if (evp_md == NULL) {
+        evp_md = EVP_sha256();
     }
 
-    HMAC(evp_md, signing_key->data, signing_key->len, blob->data, blob->len, md, &md_len);
-	retval->data = ngx_palloc(pool, md_len * 2 + 1);
-	retval->len = md_len * 2;
-	ngx_hex_dump(retval->data, md, md_len);
-	return retval;
+    EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(mdctx, evp_md, NULL);
+    EVP_DigestUpdate(mdctx, blob->data, blob->len);
+    EVP_DigestFinal_ex(mdctx, md, &md_len);
+    EVP_MD_CTX_free(mdctx);
+
+    retval->data = ngx_palloc(pool, md_len * 2 + 1);
+    retval->len = md_len * 2;
+    ngx_hex_dump(retval->data, md, md_len);
+    return retval;
 }
 
 ngx_str_t* ngx_aws_auth__hash_sha256(ngx_pool_t *pool, const ngx_str_t *blob) {
     unsigned char hash[SHA256_DIGEST_LENGTH];
-	ngx_str_t *const retval = ngx_palloc(pool, sizeof(ngx_str_t));
+    ngx_str_t *const retval = ngx_palloc(pool, sizeof(ngx_str_t));
 
-    SHA256_CTX sha256;
-    SHA256_Init(&sha256);
-    SHA256_Update(&sha256, blob->data, blob->len);
-    SHA256_Final(hash, &sha256);
+    EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(mdctx, EVP_sha256(), NULL);
+    EVP_DigestUpdate(mdctx, blob->data, blob->len);
+    EVP_DigestFinal_ex(mdctx, hash, NULL);
+    EVP_MD_CTX_free(mdctx);
 
     retval->data = ngx_palloc(pool, SHA256_DIGEST_LENGTH * 2 + 1);
     retval->len = SHA256_DIGEST_LENGTH * 2;
-	ngx_hex_dump(retval->data, hash, sizeof(hash));
-	return retval;
+    ngx_hex_dump(retval->data, hash, sizeof(hash));
+    return retval;
 }
